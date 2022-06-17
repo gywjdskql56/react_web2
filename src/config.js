9f28232e46ba1866d455c6a6dc08984447646496
hyojeong_kim <gywjdskql56@gmail.com>
2022-06-17 13:49:34 +0900

    puysh

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 global.XMLHttpRequest = require("xhr2");
-const url = "http://3.34.179.174/:5000";
+
+const url = "http://3.34.179.174:5000";
 export default function httpGet(theURL) {
   const xmlHttp = new XMLHttpRequest();
   console.log(url.concat(theURL));
