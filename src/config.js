f92be9055f486fe7f2adb2720be78d548434bc65
hyojeong_kim <gywjdskql56@gmail.com>
2022-07-01 09:23:37 +0900

    feat: change url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 global.XMLHttpRequest = require("xhr2");
 
-// const url = "http://15.164.162.233:5000";
-const url = "http://172.16.120.19:5000/";
+ const url = "http://13.209.7.71:5000";
+//const url = "http://172.16.120.19:5000/";
 export default function httpGet(theURL) {
   const xmlHttp = new XMLHttpRequest();
   console.log(url.concat(theURL));
