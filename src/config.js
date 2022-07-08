d7823a2a9c3f5679ff16415564e3a729e94e8aa8
hyojeong_kim <gywjdskql56@gmail.com>
2022-07-08 15:33:02 +0900

    fix url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.XMLHttpRequest = require("xhr2");
 
 // const url = "http://43.200.4.182:5000";
 // const url = "http://172.16.120.19:5000/";
-const url = "http://127.0.0.1:5000";
+const url = "http://43.200.4.182:5000";
 export default function httpGet(theURL) {
   const xmlHttp = new XMLHttpRequest();
   console.log(url.concat(theURL));
