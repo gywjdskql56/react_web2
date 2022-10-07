580b1162a1607f6c3936cf3322f2c3759fcef124
hyojeong_kim <hyojeong_kim@miraeasset.com>
2022-10-07 14:24:48 +0900

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 global.XMLHttpRequest = require("xhr2");
 
- const url = "http://172.16.120.19:5000";
+// const url = "http://172.16.120.19:5000";
+const url = "http://43.200.170.131:5000";
 // const url = "http://210.182.168:5000";
 // const url = "http://54.180.86.233:5001";
 export default function httpGet(theURL) {
